45b958696b99363bed5fcec487cffec5811692c2
Alice-63 <alicetindag84@gmail.com>
2021-07-02 22:47:48 +0200

    created

---
+++ script.js
@@ -0,0 +1,21 @@
+// const collapse= document.getElementById("collapse");
+
+// let offen= true;
+
+
+function col(button)
+{
+    let parent=button.parentElement;
+    let icon=button.children[0];
+
+   if(parent.offsetHeight==64)
+   {
+    icon.style.transform="rotate(180deg)";
+    parent.style.height="100px"
+   }
+   else
+   {
+    icon.style.transform="rotate(0deg)"
+    parent.style.height="64px"
+   }
+}
